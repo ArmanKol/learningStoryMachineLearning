46a8e56231db0ca9ae7d5e3c81fb60b80996d0f2
ArmanKol <arman.koldaguc@gmai.com>
2020-01-30 20:06:57 +0100

    machinelearning resultaat slaat nu een rule op

--- Main.py
+++ Main.py
@@ -1,27 +1,30 @@
 import json
 import requests
 import pandas as pd
-from datetime import datetime
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+from random import randrange
 
 def get_sec(time_str):
     h, m, s = time_str.split(':')
     return int(h) * 3600 + int(m) * 60 + int(s)
-
 
 def convert(seconds):
     min, sec = divmod(seconds, 60)
     hour, min = divmod(min, 60)
     return "%d:%02d:%02d" % (hour, min, sec)
 
-def postRule(postObject):
-    print()
-
+def opslaanRule():
+    ruleGenereren(runDecisionThree(1, 0, 0), "A", "ON")
+    ruleGenereren(runDecisionThree(0, 0, 0), "B", "OFF")
 
 def ruleGenereren(tijd, ruleAB, status):
     URL = 'http://localhost:8080/rest/rules/'
+    randomID = randrange(50000)
 
     data = {}
     actions = {}
@@ -30,14 +33,14 @@ def ruleGenereren(tijd, ruleAB, status):
     configurationTrigger = {}
     configurationAction = {}
 
-    triggers['id'] = 2
+    triggers['id'] = randomID
     triggers['label'] = "it is a fixed time of day"
     triggers['description'] = "Triggers at a specified time"
     configurationTrigger['time'] = tijd
     triggers['configuration'] = configurationTrigger
     triggers['type'] = "timer.TimeOfDayTrigger"
 
-    actions['id'] = 3
+    actions['id'] = randomID+1
     actions['label'] = "send a command"
     actions['description'] = "Sends a command to a specified item."
     configurationAction['itemName'] = "bathroomWashingMachine_1_Relay"
@@ -47,74 +50,126 @@ def ruleGenereren(tijd, ruleAB, status):
 
     data['triggers'] = [triggers]
     data['actions'] = [actions]
-    data['name'] = "TESTT"+"_"+ruleAB
+    data['name'] = "MachineLearning"+"_"+ruleAB
 
     json_data = json.dumps(data)
     requests.post(URL, data=json_data, headers={"content-type": "application/json"})
 
 def runKnn():
     learningData = pd.read_csv("test.csv")
-    df = pd.DataFrame(learningData)
-    origineel = pd.DataFrame(learningData)
 
-    df['datum'] = df['datum'].astype('datetime64')
-    df['tijd'] = df['datum'].dt.time
-    df['datum'] = df['datum'].dt.date
-    df['tijd'] = df['tijd'].astype('str')
-    print(df.dtypes)
+    learningData['datum'] = learningData['datum'].astype('datetime64')
+    learningData['tijd'] = learningData['datum'].dt.time
+    learningData['datum'] = learningData['datum'].dt.date
+    learningData['tijd'] = learningData['tijd'].astype('str')
+    print(learningData.dtypes)
 
     seconds = []
 
-    for i in df['tijd']:
+    for i in learningData['tijd']:
         seconds.append(get_sec(i))
 
-    for j in df['stand']:
+    for j in learningData['stand']:
         if ('ON' in j):
-            df['stand'] = df['stand'].str.replace('ON', '1')
+            learningData['stand'] = learningData['stand'].str.replace('ON', '1')
         if ('OFF' in j):
-            df['stand'] = df['stand'].str.replace('OFF', '0')
+            learningData['stand'] = learningData['stand'].str.replace('OFF', '0')
 
-    for k in df['datum']:
-        df['dag'] = k.weekday()+1
+    for k in learningData['datum']:
+        learningData['dag'] = k.weekday()+1
 
 
-    df['seconds'] = seconds
+    learningData['seconds'] = seconds
+    learningData = learningData.drop(columns="datum")
 
     dict_switch = dict(zip(learningData['id'].unique(),learningData['stand'].unique()))
     print("dict_switch:")
     print(dict_switch)
 
-    print(df.head())
-
+    print(learningData.head())
     #X = input, Y=output
     X=learningData[['stand', 'dag', 'kamer']]
     y=learningData['seconds']
 
     # splits in train en test set
-    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state = 100)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state = 0)
     print('Aantal trainwaarden {0:d}'.format(len(X_train)))
     print('Aantal testwaarden {0:d}'.format(len(y_test)))
 
-    # Het indelen van groepen x aantal groepen
-    knn = KNeighborsClassifier(n_neighbors = 3)
-    # Trainen van de data
-    knn.fit(X_train,y_train)
-    # laat de classifier de testset berekenen
-    y_knn = knn.predict(X_test)
-    # controleer de nauwkeurigheid met de test data
-    print("knn.score:")
-    print(knn.score(X_test,y_test))
+    k_range = range(1,26)
+    scores = {}
+    scores_list = []
+
+    for k in k_range:
+        # Het indelen van groepen x aantal groepen
+        knn = KNeighborsClassifier(n_neighbors=2)
+        # Trainen van de data
+        knn.fit(X_train, y_train)
+        # laat de classifier de testset berekenen
+        y_knn = knn.predict(X_test)
+        # controleer de nauwkeurigheid met de test data
+        scores[k] = metrics.accuracy_score(y_test, y_knn)
+        scores_list.append(metrics.accuracy_score(y_test, y_knn))
+
+    plt.plot(k_range, scores_list)
+    plt.xlabel('value of K for KNN')
+    plt.ylabel('Tesint Accuracy')
+    #plt.show()
 
     # controleer met een confusion matrix
     cm = confusion_matrix(y_test,y_knn)
     print(cm)
 
-    # voorspel voor een bepaalde vrucht seconds, dag, kamer
+    # voorspel voor een bepaalde seconds, dag, kamer
     stand_prediction = knn.predict([[0, 0, 0]])
-    #print("Stand_prediction:")
-    #print(stand_prediction)
+    print("Stand_prediction:")
 
     #print(dict_switch[stand_prediction[0]])
     print(convert(stand_prediction[0]))
 
-runKnn()
+def runDecisionThree(status, dag, kamer):
+    learningData = pd.read_csv("test.csv")
+
+    learningData['datum'] = learningData['datum'].astype('datetime64')
+    learningData['tijd'] = learningData['datum'].dt.time
+    learningData['datum'] = learningData['datum'].dt.date
+    learningData['tijd'] = learningData['tijd'].astype('str')
+
+    seconds = []
+
+    for i in learningData['tijd']:
+        seconds.append(get_sec(i))
+
+    for j in learningData['stand']:
+        if ('ON' in j):
+            learningData['stand'] = learningData['stand'].str.replace('ON', '1')
+        if ('OFF' in j):
+            learningData['stand'] = learningData['stand'].str.replace('OFF', '0')
+
+    for k in learningData['datum']:
+        learningData['dag'] = k.weekday() + 1
+
+    learningData['seconds'] = seconds
+    learningData = learningData.drop(columns="datum")
+
+    # X = input, Y=output
+    X = learningData[['stand', 'dag', 'kamer']]
+    y = learningData['seconds']
+
+    # splits in train en test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    print('Aantal trainwaarden {0:d}'.format(len(X_train)))
+    print('Aantal testwaarden {0:d}'.format(len(y_test)))
+
+    tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+    tree_clf.fit(X, y)
+
+    print("Score:")
+    print(tree_clf.score(X_train, y_train))
+
+    # voorspel tijd met stand, dag, kamer
+    stand_prediction = tree_clf.predict([[status, dag, kamer]])
+
+    return convert(stand_prediction[0])
+
+opslaanRule()
--- Tensorflow.py
+++ Tensorflow.py
@@ -9,3 +9,86 @@ from tensorflow import feature_column
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 
+def get_sec(time_str):
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+  dataframe = dataframe.copy()
+  labels = dataframe.pop('seconds')
+  ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(dataframe))
+  ds = ds.batch(batch_size)
+  return ds
+
+learningData = pd.read_csv("test.csv")
+
+learningData['datum'] = learningData['datum'].astype('datetime64')
+learningData['tijd'] = learningData['datum'].dt.time
+learningData['datum'] = learningData['datum'].dt.date
+learningData['tijd'] = learningData['tijd'].astype('str')
+
+seconds = []
+
+for i in learningData['tijd']:
+    seconds.append(get_sec(i))
+
+for j in learningData['stand']:
+    if ('ON' in j):
+        learningData['stand'] = learningData['stand'].str.replace('ON', '1')
+    if ('OFF' in j):
+        learningData['stand'] = learningData['stand'].str.replace('OFF', '0')
+
+for k in learningData['datum']:
+    learningData['dag'] = k.weekday()+1
+
+learningData['stand'] = learningData['stand'].astype('int64')
+learningData['seconds'] = seconds
+learningData = learningData.drop(columns="datum")
+learningData = learningData.drop(columns="tijd")
+
+train, test = train_test_split(learningData, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
+
+#Input pipeline
+batch_size = 32 # A small batch sized is used for demonstration purposes
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+print(train_ds)
+
+for feature_batch, label_batch in train_ds.take(1):
+  print('Every feature:', list(feature_batch.keys()))
+  #print('A batch of ages:', feature_batch['seconds'])
+  print('A batch of targets:', label_batch )
+
+
+feature_columns = []
+
+# numeric cols
+for header in ['kamer', 'stand', 'dag']:
+  feature_columns.append(feature_column.numeric_column(header))
+
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+model = tf.keras.Sequential([
+  feature_layer,
+  layers.Dense(128, activation='relu'),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_ds,
+          validation_data=val_ds,
+          epochs=5)
